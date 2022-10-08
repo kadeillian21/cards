ea4085f7f3413d3017157e77b4162fade30e3058
Kade Illian <kadeillianmt@gmail.com>
2022-10-08 11:21:12 -0600

    added save and load methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -34,4 +34,17 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
 end
+
+# Pattern matching is Elixir's replacement for variable assignment.
+# This means that any time you 'assign a variable' in Elixir, you are really engaging in pattern matching.  'one = 1' is the simplest form of pattern matching.
